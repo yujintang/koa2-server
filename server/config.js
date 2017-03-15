9cf41cf6f9bba8e680dc5ad29f5aca888c737548
余金堂 <shanquan54@163.com>
2017-03-15 18:51:07 +0800

    config

--- server/config.js
+++ server/config.js
@@ -6,7 +6,7 @@ const configure = function () {
     const fs = require('fs');
 
     var port,
-        PROJECT_NAME = 'quehui',
+        PROJECT_NAME = '**',
         mongo, redis, github, qq, wx;
 
     switch (process.env.NODE_ENV) {
@@ -15,68 +15,37 @@ const configure = function () {
             break;
         case 'real':
             port = 3000;
-            mongo = {
-                host: '127.0.0.1',
-                port: '27017',
-                db: 'quehui',
-                user: 'mongo',
-                pass: 'mongo'
-            };
-            redis = {
-                host: '127.0.0.1',
-                port: '6379',
-                db: 0,
-                pass: 'redis666'
-            };
-            github = {
-                client_id: '5cfe2b646f07fe4c8b79',
-                client_secret: '30d46df6bce48b358218d675f124148b8b1f407f',
-                redirect_url: 'http://www.7diary.com/qh_api/auth/githubCb',
-                home_url: 'http://www.7diary.com/qh_back/#/home'
-            };
-            qq = {
-                app_id: '101382837',
-                app_key: 'b3c014b9676ab3cc380479f447652944',
-                redirect_url: 'http://www.7diary.com/qh_api/auth/qqCb',
-                home_url: 'http://www.7diary.com/qh_back/#/home'
-            };
-            wx = {
-                app_id: 'wxeaacd3ecb412444a',
-                app_key: '6322087ebc1267c32e9412702e5257dd',
-                token: 'yujintang'
-            };
             break;
         default:
             port = 12121;
             mongo = {
-                host: '120.27.115.24',
-                port: '27017',
-                db: 'quehui',
-                user: 'mongo',
-                pass: 'mongo'
+                host: '**',
+                port: '**',
+                db: '**',
+                user: '**',
+                pass: '**'
             };
             redis = {
-                host: '120.27.115.24',
-                port: '6379',
+                host: '**',
+                port: '**',
                 db: 0,
-                pass: 'redis666'
+                pass: '**'
             };
             github = {
-                client_id: '180b0603101d0a22aaeb',
-                client_secret: '86a6a16dc1711ae2f7d5a12babb2e3f114c9b4de',
-                redirect_url: 'http://192.168.3.109:8080/qh/auth/githubCb',
-                home_url: 'http://192.168.3.109:8080/#/home'
+                client_id: '**',
+                client_secret: '**',
+                redirect_url: '**',
+                home_url: '**'
             };
             qq = {
-                app_id: '101382837',
-                app_key: 'b3c014b9676ab3cc380479f447652944',
-                redirect_url: 'http://127.0.0.1:12121/auth/qqCb',
-                home_url: 'http://192.168.3.109:8080/#/home'
+                app_id: '**',
+                app_key: '**',
+                redirect_url: '**'
             };
             wx = {
-                app_id: 'wxeaacd3ecb412444a',
-                app_key: '6322087ebc1267c32e9412702e5257dd',
-                token: 'yujintang'
+                app_id: '**',
+                app_key: '**',
+                token: '**'
             };
             break;
     }
@@ -95,11 +64,11 @@ const configure = function () {
             upload: path.resolve(process.cwd(), 'upload')
         },
         qiniu: {
-            AK: 'UFmgpsHCF5goS2eEk3-CYiTHiF6KOqCqsi4C2R8Y',
-            SK: '40HFTpsLjHpZ-7udjoRbqI-zkiLTIrcf1jJGvAQ8',
-            bucket: '7diary',
-            domain_url: 'http://images.7diary.com/',
-            avatar_style: 'imageView2/1/w/200/h/200/interlace/0/q/100'
+            AK: '**',
+            SK: '**',
+            bucket: '**',
+            domain_url: '**',
+            avatar_style: '**'
         },
         mail: {
             notice: {
@@ -107,8 +76,8 @@ const configure = function () {
                 port: 465,
                 secureConnection: true, // 使用 SSL
                 auth: {
-                    user: 'vip@7diary.com',
-                    pass: 'qH1234567'
+                    user: '**',
+                    pass: '**'
                 }
             }
         },
@@ -118,7 +87,7 @@ const configure = function () {
             key: fs.readFileSync(path.join(__dirname, '../https/privatekey.pem')),
             cert: fs.readFileSync(path.join(__dirname, '../https/certificate.pem'))
         },
-        wx: wx
+        wx:wx
     };
 
     //目录不存在，则创建
